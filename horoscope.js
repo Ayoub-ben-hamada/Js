c4b859602ab1089e133cee594949f043cd50169e
tibou-25 <ayoubbenhamada2@gmail.com>
2025-04-07 16:54:40 +0100

    added a new js file

---
+++ horoscope.js
@@ -0,0 +1,29 @@
+let months = "January";
+
+
+if (months == "January") {console.log("You are a Capricorn ♑");}
+else if (months == "February"){
+    console.log("You are an Aquarius ♒");   
+}else if (months == "March"){
+    console.log("You are a Pisces ♓");
+}else if (months == "April"){
+    console.log("You are an Aries ♈");
+}else if (months == "May"){
+    console.log("You are a Taurus ♉");
+}else if (months == "June"){
+    console.log("You are a Gemini ♊");
+}else if (months == "July"){
+    console.log("You are a Cancer ♋");
+}else if (months == "August"){
+    console.log("You are a Leo ♌");
+}else if (months == "September"){
+    console.log("You are a Virgo ♍");
+}else if (months == "October"){
+    console.log("You are a Libra ♎");
+}else if (months == "November"){
+    console.log("You are a Scorpio ♏");
+}else if (months == "December"){
+    console.log("You are a Sagittarius ♐");
+}else{
+    console.log("Invalid month entered");
+}
